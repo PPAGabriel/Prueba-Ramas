5ff9bc2a480dfbcc01749b0df1452cef6d6b047e
Nicolas Rodriguez Steuerberg <nrodriguezsteuerberg@danielcastelao.org>
2022-12-08 13:10:54 +0100

    Cambio en el programa

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,21 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-    /* COMMITS GABRIEL: COLORES
-    * AZUL
-    * ROJO
-    * VERDE
-    * */
+        /* COMMITS NSteuerberg: Fechas
+         * 01/04/2003
+         * 8/12/2022
+         * */
+
+        System.out.println("Soy el colaborador oficial de este programa");
+
+        /* COMMITS GABRIEL: COLORES
+         * AZUL
+         * ROJO
+         * VERDE
+         * */
     }
+
 }
